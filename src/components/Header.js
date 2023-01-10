7fdf4a8ced95a5b8fa0dcce6e07032091381df6f
Sean Milfort <seanmilfort@seanzie-macmini.arrowcave>
2023-01-09 21:05:58 -0500

    Initial Setup and Created Header.js Component

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import { Heading, Text, Avatar, Box, VStack } from "@chakra-ui/react";
+import React from "react";
+import profilepicture from '../images/seanmilfort_profile.jpg'
+
+const Header = () => {
+return (
+    <Box justifyContent="center" marginTop="5em" >
+        <VStack>
+            <Avatar size="2xl" name="Sean Milfort" src={profilepicture} />
+            <Heading size="xl">Hello, I'm Sean Milfort!</Heading>
+            <Text size="md">Click on one of my links to find out more information!</Text>
+        </VStack>
+    </Box>
+)
+}
+
+export default Header;
